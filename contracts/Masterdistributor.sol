7d0018b878b307b4a8d1b877334094df24171c47
Roccisacer <RamM@salusafe.com>
2020-10-09 05:40:22 -0400

    Update and rename SushiRestaurant.sol to Masterdistributor.sol

--- contracts/Masterdistributor.sol
+++ contracts/Masterdistributor.sol
@@ -4,19 +4,19 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
-contract SushiRestaurant {
+contract Masterdistributor {
     using SafeMath for uint256;
     event Enter(address indexed user, uint256 amount);
     event Leave(address indexed user, uint256 amount);
 
-    IERC20 public sushi;
+    IERC20 public UBQT;
 
     uint256 public reductionPerBlock;
     uint256 public multiplier;
     uint256 public lastMultiplerProcessBlock;
 
-    uint256 public accSushiPerShare;
-    uint256 public ackSushiBalance;
+    uint256 public accUBQTiPerShare;
+    uint256 public ackUBQTiBalance;
     uint256 public totalShares;
 
     struct UserInfo {
@@ -27,8 +27,8 @@ contract SushiRestaurant {
 
     mapping (address => UserInfo) public userInfo;
 
-    constructor(IERC20 _sushi, uint256 _reductionPerBlock) public {
-        sushi = _sushi;
+    constructor(IERC20 _UBQT, uint256 _reductionPerBlock) public {
+        UBQT = _UBQT;
         reductionPerBlock = _reductionPerBlock; // Use 999999390274979584 for 10% per month
         multiplier = 1e18; // Should be good for 20 years
         lastMultiplerProcessBlock = block.number;
@@ -51,56 +51,56 @@ contract SushiRestaurant {
         lastMultiplerProcessBlock = block.number;
         // Update accSushiPerShare / ackSushiBalance
         if (totalShares > 0) {
-            uint256 additionalSushi = sushi.balanceOf(address(this)).sub(ackSushiBalance);
-            accSushiPerShare = accSushiPerShare.add(additionalSushi.mul(1e12).div(totalShares));
-            ackSushiBalance = ackSushiBalance.add(additionalSushi);
+            uint256 additionalUBQT = UBQT.balanceOf(address(this)).sub(ackUBQTBalance);
+            accUBQTPerShare = accUBQTPerShare.add(additionalUBQT.mul(1e12).div(totalShares));
+            ackUBQTBalance = ackUBQTBalance.add(additionalUBQT);
         }
     }
 
     // Get user pending reward. May be outdated until someone calls cleanup.
     function getPendingReward(address _user) public view returns (uint256) {
         UserInfo storage user = userInfo[_user];
-        return user.share.mul(accSushiPerShare).div(1e12).sub(user.rewardDebt);
+        return user.share.mul(accUBQTPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Enter the restaurant. Pay some SUSHIs. Earn some shares.
     function enter(uint256 _amount) public {
         cleanup();
-        safeSushiTransfer(msg.sender, getPendingReward(msg.sender));
-        sushi.transferFrom(msg.sender, address(this), _amount);
-        ackSushiBalance = ackSushiBalance.add(_amount);
+        safeUBQTTransfer(msg.sender, getPendingReward(msg.sender));
+        UBQT.transferFrom(msg.sender, address(this), _amount);
+        ackUBQTBalance = ackUBQTBalance.add(_amount);
         UserInfo storage user = userInfo[msg.sender];
         uint256 moreShare = _amount.mul(multiplier).div(1e18);
         user.amount = user.amount.add(_amount);
         totalShares = totalShares.add(moreShare);
         user.share = user.share.add(moreShare);
-        user.rewardDebt = user.share.mul(accSushiPerShare).div(1e12);
+        user.rewardDebt = user.share.mul(accUBQTPerShare).div(1e12);
         emit Enter(msg.sender, _amount);
     }
 
     // Leave the restaurant. Claim back your SUSHIs.
     function leave(uint256 _amount) public {
         cleanup();
-        safeSushiTransfer(msg.sender, getPendingReward(msg.sender));
+        safeUBQTTransfer(msg.sender, getPendingReward(msg.sender));
         UserInfo storage user = userInfo[msg.sender];
         uint256 lessShare = user.share.mul(_amount).div(user.amount);
         user.amount = user.amount.sub(_amount);
         totalShares = totalShares.sub(lessShare);
         user.share = user.share.sub(lessShare);
-        user.rewardDebt = user.share.mul(accSushiPerShare).div(1e12);
-        safeSushiTransfer(msg.sender, _amount);
+        user.rewardDebt = user.share.mul(accUBQTPerShare).div(1e12);
+        safeUBQTTransfer(msg.sender, _amount);
         emit Leave(msg.sender, _amount);
     }
 
     // Safe sushi transfer function, just in case if rounding error causes pool to not have enough SUSHIs.
-    function safeSushiTransfer(address _to, uint256 _amount) internal {
-        uint256 sushiBal = sushi.balanceOf(address(this));
-        if (_amount > sushiBal) {
-            sushi.transfer(_to, sushiBal);
-            ackSushiBalance = ackSushiBalance.sub(sushiBal);
+    function safeUBQTTransfer(address _to, uint256 _amount) internal {
+        uint256 UBQTBal = UBQT.balanceOf(address(this));
+        if (_amount > UBQTBal) {
+            UBQT.transfer(_to, UBQTBal);
+            ackUBQTBalance = ackUBQTBalance.sub(sushiBal);
         } else {
-            sushi.transfer(_to, _amount);
-            ackSushiBalance = ackSushiBalance.sub(_amount);
+            UBQT.transfer(_to, _amount);
+            ackUBQTBalance = ackUBQTBalance.sub(_amount);
         }
     }
 }
